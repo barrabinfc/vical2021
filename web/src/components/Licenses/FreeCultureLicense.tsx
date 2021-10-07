7539bd7295ea36fa5676024654cfaa5fc4edff12
Vitor Calejuri <vitor.cajuleri@gmail.com>
2021-10-07 11:33:45 -0300

    Style: Added cc icon and set global img min-height to 32px

---
+++ web/src/components/Licenses/FreeCultureLicense.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const FreeCultureLicense = () => {
+  return (
+    <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
+      <img
+        alt="Creative Commons License"
+        width="88px"
+        height="32px"
+        src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png"
+      />
+    </a>
+  );
+};
+
+export default FreeCultureLicense;
