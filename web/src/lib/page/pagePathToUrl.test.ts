a5542cc34fcf309d9ce01e42ab04f1ee1b3a0d3c
Vitor Calejuri <vitor.cajuleri@gmail.com>
2021-10-07 19:38:15 -0300

    Fix: Fix pagePathToUrl UT to pass

--- web/src/lib/page/pagePathToUrl.test.ts
+++ web/src/lib/page/pagePathToUrl.test.ts
@@ -10,13 +10,13 @@ const abspathFixtures = [
 ];
 
 test('pagePathToUrl(projects/iching/index) should return the correct URL for index', async () => {
-  expect(pagePathToUrl(abspathFixtures[0]).pathname).to.be.equal('/projects/iching');
+  expect(pagePathToUrl(abspathFixtures[0]).pathname).to.match(/^\/projects\/iching$/);
 });
 test('pagePathToUrl(garden/complexity/introduction to complexity) should return the correct URL for index', async () => {
-  expect(pagePathToUrl(abspathFixtures[1]).pathname).to.be.equal('/garden/complexity/introduction%20to%20complexity');
+  expect(pagePathToUrl(abspathFixtures[1]).pathname).to.match(/^\/garden\/complexity\/introduction%20to%20complexity$/);
 });
 test('pagePathToUrl(projects/iching/index) should return the correct URL for index', async () => {
-  expect(pagePathToUrl(abspathFixtures[2]).pathname).to.be.equal('/garden/hipocratic-oath-for-developers');
+  expect(pagePathToUrl(abspathFixtures[2]).pathname).to.match(/^\/garden\/hipocratic-oath-for-developers$/);
 });
 
 test.run();
