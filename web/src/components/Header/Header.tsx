10eb285c171a6a12a327a8a33df657015ec659b6
Vitor Calejuri <vitor.cajuleri@gmail.com>
2021-10-21 16:34:31 -0300

    Improve header navigation style and acessibility

--- web/src/components/Header/Header.tsx
+++ web/src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Navigation from '../Navigation/Navigation';
 import { cn } from '../../lib/helpers';
 
@@ -7,41 +7,27 @@ import useWindowScrollPosition from '@rehooks/window-scroll-position';
 import Icon from '../icon';
 import styles from './Header.module.scss';
 
-export default function Header({ fixed = false }: { fixed: boolean }) {
-  const [showNav, setShowNav] = useState(false);
-  const handleShowNav = () => setShowNav(true);
-  const handleHideNav = () => setShowNav(false);
-
+export default function Header({ sticky = false }: { sticky: boolean }) {
   let headerVariant = [];
-  if (fixed && !import.meta.env.SSR) {
+  if (sticky && !import.meta.env.SSR) {
     const position = useWindowScrollPosition({ throttle: 100 });
-    headerVariant = (position.y >= 32 && ['translucent']) || [];
+    headerVariant = (position.y >= 32 && ['sticky']) || [];
   }
 
   return (
     <div className={cn(styles.header, 'header', ...headerVariant)}>
-      <div className={styles.wrapper}>
-        <a className={styles.skipMain} href="#main">
-          Skip to main content
+      <a className={styles.skipMain} href="#main">
+        Skip to main content
+      </a>
+
+      <div className={styles.branding}>
+        <a href="/">
+          <span className="visually-hidden">Home</span>
+          <Icon symbol="vical" />
         </a>
-
-        <div className={styles.branding}>
-          <a href="/">
-            <span className="visually-hidden">Home</span>
-            <Icon symbol="vical" />
-          </a>
-        </div>
-
-        <button
-          className={styles.toggleNavButton}
-          aria-label="Toggle navigation menu"
-          onClick={showNav ? handleHideNav : handleShowNav}
-        >
-          <Icon symbol="hamburger"></Icon>
-        </button>
-
-        <Navigation showNav={showNav}></Navigation>
       </div>
+
+      <Navigation />
     </div>
   );
 }
--- web/src/components/Navigation/Navigation.jsx
+++ web/src/components/Navigation/Navigation.jsx
@@ -1,19 +1,30 @@
-import React from "react";
-import { cn } from "../../lib/helpers";
+import React, { useState } from 'react';
+import { cn } from '../../lib/helpers';
 
-import styles from "./Navigation.module.scss";
+import styles from './Navigation.module.scss';
+import Icon from '../icon';
 
-export default function Navigation({ showNav }) {
+export default function Navigation() {
+  const [showNav, setShowNav] = useState(false);
+  const handleShowNav = () => setShowNav(true);
+  const handleHideNav = () => setShowNav(false);
+
   return (
-    <nav
-      aria-label="Main"
-      className={cn(styles.nav, showNav && styles.showNav)}
-    >
-      <ul>
-        <li>
+    <nav className={cn(styles.nav)}>
+      <button
+        className={styles.toggleNavButton}
+        aria-label="Toggle navigation menu"
+        aria-expanded={showNav}
+        onClick={showNav ? handleHideNav : handleShowNav}
+      >
+        <Icon symbol="hamburger"></Icon>
+      </button>
+
+      <ul role="menu" hidden={!showNav}>
+        <li role="menuitem">
           <a href="/about">About</a>
         </li>
-        <li>
+        <li role="menuitem">
           <a href="/projects">projects</a>
         </li>
       </ul>
