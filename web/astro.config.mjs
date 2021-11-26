9194098e7fabaea896737c7d7df10944859d8645
Vitor Calejuri <vitor.cajuleri@gmail.com>
2021-11-26 13:03:55 -0300

    Example gpg

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,17 +1,9 @@
-import 'module-alias/register.js';
-import { readFileSync } from 'fs';
+// @ts-check
 
-import * as shiki from 'shiki';
-
-const packageFileContent = readFileSync('./package.json', 'utf8');
-const packageInfo = JSON.parse(packageFileContent);
-const astroConfig = packageInfo._astroConfig;
-
-const highlighter = await shiki.getHighlighter({ theme: 'poimandres' });
-
-export default /** @type {import('astro').AstroUserConfig} */ ({
-  ...astroConfig,
-  markdownOptions: {
-    ...astroConfig.markdownOptions
+/** @type {import('astro').AstroUserConfig} */
+export default {
+  renderers: ['@astrojs/renderer-react'],
+  vite: {
+    plugins: []
   }
-});
+};
