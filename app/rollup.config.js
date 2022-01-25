037d0e9f5e63a108a59c27844da7deeaf89e84ef
Vitor Calejuri <barrabin.fc@gmail.com>
2022-01-25 14:42:14 -0300

    Feat: @custom-media support via service worker

---
+++ app/rollup.config.js
@@ -0,0 +1,32 @@
+// rollup.config.js
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
+import commonjs from "@rollup/plugin-commonjs";
+
+import pkg from "./package.json";
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+export default {
+  input: "app/sw.js",
+  output: {
+    dir: "public",
+    format: "cjs",
+  },
+  plugins: [
+    nodeResolve({
+      moduleDirectories: ["node_modules", "app"],
+    }),
+    commonjs({
+      transformMixedEsModules: true,
+    }),
+    replace({
+      preventAssignment: true,
+      "process.env.NODE_ENV": JSON.stringify(
+        process.env.NODE_ENV || "development"
+      ),
+      __buildVersion__: JSON.stringify(pkg.version),
+    }),
+  ],
+};
